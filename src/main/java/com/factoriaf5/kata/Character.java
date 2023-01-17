5d915464ace8677f0caa3be6c684903b03e2bb6d
SaulAguinaga <saul_aguinaga@hotmail.es>
2023-01-17 11:19:48 +0100

    class Character

--- src/main/java/com/factoriaf5/kata/Character.java
+++ src/main/java/com/factoriaf5/kata/Character.java
@@ -1,5 +1,55 @@
 package com.factoriaf5.kata;
 
 public class Character {
+
+    private int health;
+    private int level;
+    private boolean alive;
+    private String name;
     
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Character(String name) {
+        this.health = 1000;
+        this.level = 1;
+        this.alive = true;
+        this.name= name;
+    }
+
+    public void DealDamage(Character character){
+        character.setHealth(this.health-100);
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    
+
 }
