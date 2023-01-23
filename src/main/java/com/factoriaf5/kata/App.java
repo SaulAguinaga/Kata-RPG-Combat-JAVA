ec146e9d9b3b53738ad050b5043a466b299f0882
SaulAguinaga <saul_aguinaga@hotmail.es>
2023-01-23 08:46:12 +0100

    app changes

--- src/main/java/com/factoriaf5/kata/App.java
+++ src/main/java/com/factoriaf5/kata/App.java
@@ -1,16 +1,7 @@
 package com.factoriaf5.kata;
 
-/**
- * Hello world!
- */
-public final class App {
-    private App() {
-    }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
+   
     public static void main(String[] args) {
         Character ironMan = new Character("Iron Man");
         Character thanos = new Character("Thanos");
